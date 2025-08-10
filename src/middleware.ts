69b78a84347f579944c66bd86ee9f94182eec879
004dna-code <004dna@gmail.com>
2025-08-10 14:52:26 +0000

    klklk

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,7 +30,7 @@ export async function middleware(req: NextRequest) {
     },
   );
 
-  // Refresh session if expired - required for Server Components
+  // Refresh session ppif expired - required for Server Components
   const {
     data: { session },
     error,
